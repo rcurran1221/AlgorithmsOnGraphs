d2d86c5e72aa62173a5c195b20acbb2b873b866a
Robert Curran <rcurran@alumni.bentley.edu>
2019-03-30 17:39:49 -0400

    test changes

--- assignment1/reachability/test/reachability_test.py
+++ assignment1/reachability/test/reachability_test.py
@@ -1,14 +1,12 @@
 #Uses python3
 
-import sys
+#import sys
 
 def reach(adj, x, y):
     #write your code here
     return 0
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+def explore(data):
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
@@ -19,3 +17,18 @@ if __name__ == '__main__':
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
     print(reach(adj, x, y))
+
+if __name__ == '__main__':
+    caseOne = [4, 4,
+               1, 2,
+               3, 2,
+               4, 3,
+               1, 4,
+               1, 4,]
+    explore(caseOne)
+    
+    caseTwo = [4, 2,
+               1, 2,
+               3, 2,
+               1, 4]
+    explore(caseTwo)
