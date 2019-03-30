f3e7920c66c82d13a6eafbb63d0f1da41d7b8bbf
Robert Curran <rcurran@alumni.bentley.edu>
2019-03-30 18:10:28 -0400

    reachability test file with 4 test cases

--- assignment1/reachability/test/reachability_test.py
+++ assignment1/reachability/test/reachability_test.py
@@ -2,12 +2,27 @@
 
 #import sys
 
-def reach(adj, x, y):
-    #write your code here
-    return 0
+visited = []
+adj = []
+verticiesCount = 0
 
-def explore(data):
+def reach(x, y):
+    explore(x)    
+    return 1 if visited[y] else 0
+
+def explore(v):
+    visited[v] = True
+    for y in range(len(adj[v])):
+        if not visited[adj[v][y]]:
+            explore(adj[v][y])
+    
+def canExitMaze(data):
+    global adj
+    global verticiesCount
+    global visited
     n, m = data[0:2]
+    verticiesCount = n
+    visited = [False for _ in range(verticiesCount)]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
     x, y = data[2 * m:]
@@ -16,7 +31,7 @@ def explore(data):
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
-    print(reach(adj, x, y))
+    print(reach(x, y))
 
 if __name__ == '__main__':
     caseOne = [4, 4,
@@ -25,10 +40,31 @@ if __name__ == '__main__':
                4, 3,
                1, 4,
                1, 4,]
-    explore(caseOne)
+    canExitMaze(caseOne)
     
     caseTwo = [4, 2,
                1, 2,
                3, 2,
                1, 4]
-    explore(caseTwo)
+    canExitMaze(caseTwo)
+
+    caseThree = [7, 5,
+                 0, 2,
+                 2, 3,
+                 3, 4,
+                 1, 5,
+                 5, 6,
+                 0, 5]
+    canExitMaze(caseThree)
+    
+    caseFour =  [7, 5,
+                 0, 2,
+                 2, 3,
+                 3, 4,
+                 1, 5,
+                 5, 6,
+                 2, 4]
+    canExitMaze(caseFour)
+    
+    
+    
